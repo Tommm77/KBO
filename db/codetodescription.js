be55708f672d0bde6b8b35d3e6d088ae203ce6e8
Tommm77 <picout.tom@gmail.com>
2024-09-12 09:20:17 +0200

    script to prepare data and inject in db

---
+++ db/codetodescription.js
@@ -0,0 +1,89 @@
+const fs = require('fs');
+const path = require('path');
+const JSONStream = require('JSONStream');
+const es = require('event-stream');
+
+const langFr = require('./langFR.json');
+
+function replaceCodes(obj, langFr) {
+    const categories = [
+        'ActivityGroup',
+        'Classification',
+        'ContactType',
+        'EntityContact',
+        'JuridicalForm',
+        'JuridicalSituation',
+        'Language',
+        'Nace2003',
+        'Nace2008',
+        'Status',
+        'TypeOfAddress',
+        'TypeOfDenomination'
+    ];
+
+    function traverseAndReplace(obj) {
+        if (typeof obj === 'object' && obj !== null) {
+            for (const key in obj) {
+                if (categories.includes(key) && langFr[key] && langFr[key][obj[key]]) {
+                    obj[key] = langFr[key][obj[key]];
+                }
+
+                if (typeof obj[key] === 'object') {
+                    traverseAndReplace(obj[key]);
+                }
+            }
+        }
+    }
+
+    traverseAndReplace(obj);
+    return obj;
+}
+
+const inputFilePath = path.join(__dirname, './entreprise_updated.json');
+const outputFilePath = path.join(__dirname, 'entreprise_updated_translate.json');
+
+const readStream = fs.createReadStream(inputFilePath, { encoding: 'utf8' });
+const writeStream = fs.createWriteStream(outputFilePath, { flags: 'w' });
+
+writeStream.write('[\n');
+
+let isFirst = true;
+let counter = 0;
+
+console.log('Début du traitement...');
+
+readStream
+    .pipe(JSONStream.parse('*'))
+    .pipe(es.mapSync((enterprise) => {
+        enterprise = replaceCodes(enterprise, langFr);
+
+        if (!isFirst) {
+            writeStream.write(',\n');
+        }
+        isFirst = false;
+
+        counter++;
+        if (counter % 1000 === 0) {
+            console.log(`${counter} entreprises traitées...`);
+        }
+
+        return JSON.stringify(enterprise, null, 2);
+    }))
+    .pipe(es.through(function (data) {
+        this.emit('data', data);
+    }))
+    .pipe(writeStream);
+
+readStream.on('end', () => {
+    writeStream.write('\n]');
+    writeStream.end();
+    console.log('Le fichier JSON mis à jour a été généré avec succès.');
+});
+
+readStream.on('error', (err) => {
+    console.error('Erreur lors de la lecture du fichier JSON:', err);
+});
+
+writeStream.on('error', (err) => {
+    console.error('Erreur lors de l\'écriture du fichier JSON:', err);
+});
